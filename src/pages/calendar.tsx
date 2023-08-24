68e3595c89a7c353506884824edba978e8e27f1d
ylcharan <ylcharan99@gmail.com>
2023-08-25 04:04:21 +0530

    fixed the calendar issues

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -16,6 +16,7 @@ import Image from "next/image";
 import moment from "moment";
 import listPlugin from "@fullcalendar/list";
 import SimpleBar from "simplebar-react";
+import { BarLoader } from "react-spinners";
 
 interface CalenderPageProps {}
 
@@ -26,6 +27,7 @@ const CalenderPage: FC<CalenderPageProps> = (props) => {
     data: [] as any[],
     activeMonth: moment(),
   });
+  const [show, setShow] = useState(false);
   const { data, activeMonth, loading } = state;
 
   function parseDate(input: string) {
@@ -83,89 +85,110 @@ const CalenderPage: FC<CalenderPageProps> = (props) => {
     fetchData();
   }, [activeYear]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 5000);
+  }, []);
+
   return (
     <Layout {...props}>
-      <SimpleBar style={{}}>
-        <div className="base-wrapper pb-[140px]">
-          <div className="mt-[50px] relative">
-            {loading && (
-              <div className="w-full h-full bg-black bg-opacity-30 absolute top-0 left-0 z-[2] flex items-center justify-center">
-                <ReactLoading
-                  type="spokes"
-                  color={"#137160"}
-                  height={100}
-                  width={100}
-                />
-              </div>
-            )}
-            <div className="relative flex items-center justify-between mb-[25px] md:mb-[50px]">
-              <div
-                onClick={() => {
-                  ref?.current?.calendar?.prev();
-                  setState((prevState) => ({
-                    ...prevState,
-                    activeMonth: moment(activeMonth).subtract(1, "months"),
-                  }));
-                }}
-                className="flex gap-[30px] items-center text-black py-[10px] px-[20px] rounded-[44px] border-black cursor-pointer hover:opacity-50 transition-opacity"
-              >
-                <Image
-                  src={ArrowRight}
-                  alt=""
-                  className="w-[24px] h-[24px] md:w-[56px] md:h-[56px]"
-                />
-                <h2 className="hidden md:block text-[32px]">
-                  {moment(activeMonth).subtract(1, "months").format("MMM")}
-                </h2>
-              </div>
-              <h2
-                key={`${activeMonth.toString()}`}
-                className="text-[22px] md:text-[64px] font-[300]"
-              >
-                {ref?.current?.calendar?.currentData?.viewTitle}
-              </h2>
-              <div
-                onClick={() => {
-                  ref?.current?.calendar?.next();
-                  setState((prevState) => ({
-                    ...prevState,
-                    activeMonth: moment(activeMonth).add(1, "months"),
-                  }));
-                }}
-                className="flex gap-[30px] items-center text-black py-[10px] px-[20px] rounded-[44px] cursor-pointer hover:opacity-50 transition-opacity"
-              >
-                <h2 className="hidden md:block text-[32px]">
-                  {moment(activeMonth).add(1, "months").format("MMM")}
-                </h2>
-                <Image
-                  src={ArrowRight}
-                  alt=""
-                  className="w-[24px] h-[24px] md:w-[56px] md:h-[56px] rotate-180"
+      {show ? (
+        <>
+          <SimpleBar style={{}}>
+            <div className="base-wrapper pb-[140px]">
+              <div className="mt-[50px] relative">
+                {loading && (
+                  <div className="w-full h-full bg-black bg-opacity-30 absolute top-0 left-0 z-[2] flex items-center justify-center">
+                    <ReactLoading
+                      type="spokes"
+                      color={"#137160"}
+                      height={100}
+                      width={100}
+                    />
+                  </div>
+                )}
+                <div className="relative flex items-center justify-between mb-[25px] md:mb-[50px]">
+                  <div
+                    onClick={() => {
+                      ref?.current?.calendar?.prev();
+                      setState((prevState) => ({
+                        ...prevState,
+                        activeMonth: moment(activeMonth).subtract(1, "months"),
+                      }));
+                    }}
+                    className="flex gap-[30px] items-center text-black py-[10px] px-[20px] rounded-[44px] border-black cursor-pointer hover:opacity-50 transition-opacity"
+                  >
+                    <Image
+                      src={ArrowRight}
+                      alt=""
+                      className="w-[24px] h-[24px] md:w-[56px] md:h-[56px]"
+                    />
+                    <h2 className="hidden md:block text-[32px]">
+                      {moment(activeMonth).subtract(1, "months").format("MMM")}
+                    </h2>
+                  </div>
+                  <h2
+                    key={`${activeMonth.toString()}`}
+                    className="text-[22px] md:text-[64px] font-[300]"
+                  >
+                    {ref?.current?.calendar?.currentData?.viewTitle}
+                  </h2>
+                  <div
+                    onClick={() => {
+                      ref?.current?.calendar?.next();
+                      setState((prevState) => ({
+                        ...prevState,
+                        activeMonth: moment(activeMonth).add(1, "months"),
+                      }));
+                    }}
+                    className="flex gap-[30px] items-center text-black py-[10px] px-[20px] rounded-[44px] cursor-pointer hover:opacity-50 transition-opacity"
+                  >
+                    <h2 className="hidden md:block text-[32px]">
+                      {moment(activeMonth).add(1, "months").format("MMM")}
+                    </h2>
+                    <Image
+                      src={ArrowRight}
+                      alt=""
+                      className="w-[24px] h-[24px] md:w-[56px] md:h-[56px] rotate-180"
+                    />
+                  </div>
+                </div>
+                <FullCalendar
+                  height="auto"
+                  ref={ref}
+                  eventDidMount={(event) => {
+                    if (event.event.extendedProps.description) {
+                      return tippy(event.el, {
+                        content: event.event.extendedProps.description,
+                      });
+                    }
+
+                    return null;
+                  }}
+                  headerToolbar={{ left: "", right: "", center: "" }}
+                  initialView={"dayGridMonth"}
+                  hiddenDays={[]}
+                  plugins={[dayGridPlugin, listPlugin]}
+                  events={data}
+                  // eventContent={renderEventContent}
                 />
               </div>
             </div>
-            <FullCalendar
-              height="auto"
-              ref={ref}
-              eventDidMount={(event) => {
-                if (event.event.extendedProps.description) {
-                  return tippy(event.el, {
-                    content: event.event.extendedProps.description,
-                  });
-                }
-
-                return null;
-              }}
-              headerToolbar={{ left: "", right: "", center: "" }}
-              initialView={"dayGridMonth"}
-              hiddenDays={[]}
-              plugins={[dayGridPlugin, listPlugin]}
-              events={data}
-              // eventContent={renderEventContent}
-            />
+          </SimpleBar>
+        </>
+      ) : (
+        <div className="min-h-[80vh]">
+          <div className="fixed top-0 flex items-center flex-col justify-center left-0 w-[100%] h-screen bg-black z-50 bg-opacity-30">
+            <div className="bg-white p-[50px] md:p-[100px] flex items-center gap-[20px] flex-col justify-center rounded-[10px]">
+              <div className="text-[#007be9] text-[22px] text-center">
+                Loading the calender
+              </div>
+              <BarLoader color="#007be9" />
+            </div>
           </div>
         </div>
-      </SimpleBar>
+      )}
     </Layout>
   );
 };
